d3146085f4c2a8fa78ae5d5c6493ed275646a90b
NoNapz <robin1robin@hotmail.com>
2022-06-27 07:50:49 +0200

    Angular Fundamentals

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './event-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
